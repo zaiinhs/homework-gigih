211591dacbf1db675d8353e7bef202528a65ac12
Zainal Abidin <zainalabidinhsc@gmail.com>
2022-03-25 22:32:24 +0700

    feat: add exercise module 2 session 3

--- src/components/Gif/index.tsx
+++ src/components/Gif/index.tsx
@@ -1,10 +1,30 @@
+import SearchBar from '../Search'
+import ImageBar from '../Image'
 import gif from '../../store/gif'
+import ButtonElement from '../Buttons'
 
 function Gif(){
     return(
         <>
-            <img src={gif.url} alt={gif.title}/>
-            <p>{gif.title}</p>
+            <div>
+                <div>
+                    <SearchBar/>
+                    <ButtonElement/>
+                </div>
+                    <div style={{display: "flex"}}>
+                        {gif.map( e => {
+                            return(
+                                <div style={{padding: "40px"}} key={e.id}>
+                                    <img src={e.url} alt={e.title}/><br />
+                                    <span style={{backgroundColor: "yellow", padding: "10px"}}>{e.rating}</span>
+                                    <p>{e.title}</p>
+                                </div>
+                                )
+
+                            })
+                        }
+                    </div>
+            </div>
 
         </>
     )
